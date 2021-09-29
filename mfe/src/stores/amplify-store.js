314f6f2140fddcda09c0a512773ee88075d64f3e
nigel447 <nigel447@gmail.com>
2021-09-29 22:10:54 +0300

    initial commit

--- mfe/src/stores/amplify-store.js
+++ mfe/src/stores/amplify-store.js
@@ -1,28 +1,59 @@
 import { Amplify } from 'aws-amplify'
+
+const isLocal = false
+
 const fedAuth = {
-    domain: "nigelsavage.auth.eu-west-1.amazoncognito.com",
-    scope: ["email", "profile", "openid"],
-    redirectSignIn: "http://localhost:3000/socialSignIn",
-    redirectSignOut: "http://localhost:3000",
-    responseType: "token"
-  }
+  domain: "nigelsavage.auth.eu-west-1.amazoncognito.com",
+  scope: ["email", "profile", "openid"],
+  redirectSignIn: "http://localhost:3000/socialSignIn",
+  redirectSignOut: "http://localhost:3000",
+  responseType: "token"
+}
 
-  const auth = {
-    mandatorySignIn: true,
-    region: 'eu-west-1',
-    userPoolId: "eu-west-1_9vfpdtgRr",
-    userPoolWebClientId: "7ka889jkncipm2kbm7o8iall26",
-    domain: "nigelsavage.auth.eu-west-1.amazoncognito.com",
-    scope: ["email", "profile", "openid"],
-    redirectSignIn: "http://localhost:3000/socialSignIn",
-    redirectSignOut: "http://localhost:3000",
-    responseType: "token"
-  }
+const fedAuthDeployed = {
+  domain: "nigelsavage.auth.eu-west-1.amazoncognito.com",
+  scope: ["email", "profile", "openid"],
+  redirectSignIn: "https://edu.nigelsavage.com/socialSignIn",
+  redirectSignOut: "https://edu.nigelsavage.com",
+  responseType: "token"
+}
 
-  export const ConfigureAmplifyStore = () => {
+const auth = {
+  mandatorySignIn: true,
+  region: 'eu-west-1',
+  userPoolId: "eu-west-1_9vfpdtgRr",
+  userPoolWebClientId: "7ka889jkncipm2kbm7o8iall26",
+  domain: "nigelsavage.auth.eu-west-1.amazoncognito.com",
+  scope: ["email", "profile", "openid"],
+  redirectSignIn: "http://localhost:3000/socialSignIn",
+  redirectSignOut: "http://localhost:3000",
+  responseType: "token"
+}
+
+const authDeployed = {
+  mandatorySignIn: true,
+  region: 'eu-west-1',
+  userPoolId: "eu-west-1_9vfpdtgRr",
+  userPoolWebClientId: "7ka889jkncipm2kbm7o8iall26",
+  domain: "nigelsavage.auth.eu-west-1.amazoncognito.com",
+  scope: ["email", "profile", "openid"],
+  redirectSignIn: "https://edu.nigelsavage.com/socialSignIn",
+  redirectSignOut: "https://edu.nigelsavage.com",
+  responseType: "token"
+}
+
+export const ConfigureAmplifyStore = () => {
+  if (isLocal) {
     Amplify.configure({
-        Auth:auth,
-        oauth: fedAuth,
+      Auth: auth,
+      oauth: fedAuth,
     })
-    return true
+  } else {
+    Amplify.configure({
+      Auth: authDeployed,
+      oauth: fedAuthDeployed,
+    })
+  }
+
+  return true
 }
